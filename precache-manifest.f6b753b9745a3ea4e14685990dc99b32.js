77b1734febd5dce5ec6ffaaf78e024752bf8b848
Joeyy Hayfron <joeacq81@gmail.com>
2020-07-26 12:05:26 +0000

    Updates

---
+++ precache-manifest.f6b753b9745a3ea4e14685990dc99b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee30cede98136f63110b249ce726c5ac",
+    "url": "/monsters-roldex-react/index.html"
+  },
+  {
+    "revision": "2957b0343284e2a9c044",
+    "url": "/monsters-roldex-react/static/css/main.466b2878.chunk.css"
+  },
+  {
+    "revision": "c5156bfa7160b1190f1c",
+    "url": "/monsters-roldex-react/static/js/2.98280ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roldex-react/static/js/2.98280ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2957b0343284e2a9c044",
+    "url": "/monsters-roldex-react/static/js/main.f1df86ec.chunk.js"
+  },
+  {
+    "revision": "00537dd5b21e39c04825",
+    "url": "/monsters-roldex-react/static/js/runtime-main.5f4c70e2.js"
+  }
+]);
